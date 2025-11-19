8e95fd26c0a73ccae0b655de00f274f79e659aea
Denis Mbugua <60645043+dynamodenis@users.noreply.github.com>
2025-11-19 13:38:56 +0300

    Add command line to test message sent

--- src/main/java/com/kafka/java_kafka/JavaKafkaApplication.java
+++ src/main/java/com/kafka/java_kafka/JavaKafkaApplication.java
@@ -1,13 +1,23 @@
 package com.kafka.java_kafka;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class JavaKafkaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaKafkaApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
+		return args -> {
+			kafkaTemplate.send("notification", "Notification from Denis");
+		};
 	}
 
 }
